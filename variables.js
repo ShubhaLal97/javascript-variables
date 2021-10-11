0ef61ca1864c5665300b22659ee9c7ebd919d168
shubha <shubha07658@gmail.com>
2021-10-12 01:22:41 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,30 @@
+console.clear()
+
+var num1 = 10
+var num2 = 20
+var num3 = 40
+
+// addition
+var sum = num1 + num2 + num3
+
+console.log(sum)
+
+// substraction
+var diff = num3 - num2
+console.log(diff)
+
+// multiplication
+var mul = num2 * num3
+console.log(mul)
+
+//division
+var div = num3 / num1
+console.log(div)
+
+var divByZero = num3 / 0
+console.log(divByZero)
+console.log(typeof(divByZero))
+
+var mulByString = num3 * 'Hello'
+console.log(mulByString)
+
